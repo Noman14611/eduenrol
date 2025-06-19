2872b7c5cbbb54811c1e9e266253f7b8b42d5013
Noman14611 <noumantareen316@gmail.com>
2025-06-19 20:42:09 +0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from courses import course_section
+from enrollments import enrollment_section
+
+st.set_page_config(page_title="EduEnroll - LMS", layout="wide")
+
+st.sidebar.title("EduEnroll 📘")
+section = st.sidebar.selectbox("Choose Section", ["Course Management", "Enrollments"])
+
+if section == "Course Management":
+    course_section()
+elif section == "Enrollments":
+    enrollment_section()
 
